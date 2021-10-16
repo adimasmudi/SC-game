0b083bfd9628a1d2ab0c0e8c658ae47d978c58bc
adimasmudi <adimasmudi@gmail.com>
2021-10-16 16:35:09 +0700

    initial commit

---
+++ live/basic.js
@@ -0,0 +1,203 @@
+// console.log("Hello world!")
+// variabel -> wadah untuk menyimpan nilai tertentu
+// var
+
+// var Nama = 'Fahmi';
+// var prodi = 'teknik industri';
+// var angkatan = 2019;
+
+// let
+// const
+
+// const Nama = 'Fahmi';
+// const prodi = 'teknik industri';
+// const angkatan = 2019;
+
+// let nama = 'Masmudi';
+
+// console.log(nama)
+
+// let nama2 = 'John';
+// console.log(nama2)
+
+// let nama = 'John';
+// console.log(nama)
+
+// nama = 'Frank';
+// console.log(nama)
+
+
+// console.log("Halo perkenalkan nama saya "+Nama+", dari prodi "+prodi+", angkatan "+angkatan)
+
+
+// let nama = 'Masmudi';
+// console.log('outer',nama)
+// if(true){
+//     let nama = 'John';
+//     console.log('inner',nama)
+// }
+
+// console.log('outer bawah',nama)
+
+// let sama const
+
+
+// operator
+// aritmatik
+// logika
+// assinment
+// bitwise
+
+// string -> huruf
+// number (int,float)
+// boolean
+
+
+// const nama = 'Masmudi';
+// console.log(typeof nama)
+
+// const angka = 12;
+// console.log(typeof angka)
+
+// const stop = true;
+// console.log(typeof stop)
+
+// const num = 10;
+// const num2 = 5;
+// const hasil = num % num2;
+// console.log(hasil)
+
+// const con1 = true;
+// const con2 = false;
+// console.log(con1 || con2)
+
+// const bil = 12;
+// if(bil % 2 == 0){
+//     console.log('bilangan genap')
+// }
+
+// console.log(12 === '12')
+
+// percabangan -> if, if else if else, if else
+
+// const bil = 13;
+// if(bil % 2 == 0){
+//     console.log('bilangan genap')
+// }else{
+//     console.log('ganjil')
+// }
+
+// penentuan grade
+// grade a -> 85 - 100
+// grade b -> 70 - 85
+// garde c -> 55 - 70
+// grade d -> 40 - 55
+// grade e -> < 40
+
+// const nilai = 79;
+// let grade;
+
+// if(nilai > 85 && nilai <= 100){
+//     grade = 'A'
+// }else if(nilai > 70 && nilai <= 85){
+//     grade = 'B'
+// }else if(nilai > 55 && nilai <= 70){
+//     grade = 'C'
+// }else if(nilai > 40 && nilai <= 55){
+//     grade = 'D'
+// }else if(nilai > 0 && nilai <= 40){
+//     grade = 'E'
+// }
+
+// console.log(grade)
+
+// percangan bersarang
+
+// const num = 10;
+
+// if(num > 5){
+//     if(num % 2 == 0){
+//         console.log('bilangan lebih besar dari 5 dan genap')
+//     }
+// }
+
+// const readline = require('readline').createInterface({
+//     input : process.stdin,
+//     output : process.stdout
+// });
+
+// readline.question('Masukkan angka : ',(angka)=>{
+//     if (angka % 2 === 0){
+//         console.log('angka genap')
+//     }else{
+//         console.log('angka ganjil')
+//     }
+// })
+
+// temperature converter sederhana untuk mengonversi nilai derajat celcius ke satuan lain
+
+
+// let result;
+
+// // ambil nilai dari user
+// readline.question('Masukkan angka dalam celcius',(c)=>{
+//     // user memilih suhu
+
+//     readline.question('Pilih suhu:\n1. Reamur\n2. Farenheit\n3. Kelvin\n',(choice)=>{
+//         // proses sesuai pilihan suhu
+//         if (choice == 1){
+//             result = (4/5) * c
+//         }else if(choice == 2){
+//             result = (9/5) * (c+32)
+//         }else if(choice == 3){
+//             result = c + 273;
+//         }
+
+//         console.log(result)
+//     })
+
+// })
+
+
+
+
+
+
+
+// penamaan variabel
+// var, const, let
+// if
+
+// nama = 'A'
+// console.log(nama)
+
+// // if = 'Jika'
+// // console.log(if)
+
+// // 1hewan = 'Kucing'
+// // console.log(1hewan)
+
+// namaHewan = 'Sapi'
+// console.log(namaHewan)
+
+// namahewan = 'kucing'
+// console.log(namahewan)
+
+// nama = 'Masmudi';
+// console.log(nama)
+
+// if(true){
+//     nama = 'John'
+//     console.log(nama)
+// }
+
+// console.log(nama)
+
+
+// hoisting
+// let angka;
+// angka = 12
+// console.log(angka)
+
+const inputan = prompt('nama :')
+
